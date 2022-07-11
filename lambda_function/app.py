2e77879078f9446fcfb7ea6ee2bb40e09dc93980
Mike Nacey <mike-nacey@gmail.com>
2022-07-11 11:28:05 -0400

    made function generic

--- lambda_function/app.py
+++ lambda_function/app.py
@@ -6,7 +6,23 @@ import base64
 import copy
 from datetime import datetime
 from dateutil import tz
+import morph
+import re
 
+
+def get_key_tail(input_str):
+    """ Get the last part of the key """
+    last_dot = input_str.rfind(".")
+    if last_dot != -1:
+        return input_str[last_dot + 1:]
+    return input
+    
+def get_key_parent(input_str):
+    """ Get the parent to the last part of the key """
+    last_dot = input_str.rfind(".")
+    if last_dot != -1:
+        return input_str[:last_dot]
+    return input
 
 #todo: have to load this from somewhere
 def get_tenant_timezone(tenant_id: str):
@@ -16,21 +32,27 @@ def get_tenant_timezone(tenant_id: str):
     }
     return zones[tenant_id]
 
+def utc_to_local(input_date, time_zone, date_format= '%Y-%m-%dT%H:%M:%S.%f%z'):
+    """ Convert a utc string date into a local string date"""
+    input_as_timestamp = datetime.strptime(input_date, date_format).replace(tzinfo=tz.gettz("UTC"))
+    return input_as_timestamp.astimezone(time_zone).strftime(date_format)
+
 
 def transform_data(data: Any): # crap but thats what json.loads returns
     """ Add a local date for input UTC dates """
 
-    date_format = '%Y-%m-%dT%H:%M:%S.%f%z'
-    local = copy.deepcopy(data)
-
-    local['header']['eventTimestampUTC'] = local['header']['eventTimestamp']
-    event_timestamp = datetime.strptime(local['header']['eventTimestamp'], date_format)
-    event_timestamp =  event_timestamp.replace(tzinfo=tz.gettz("UTC"))
-
-    local_tz = tz.gettz(get_tenant_timezone(local['header']['tenantId']))
-    event_timestamp_local = event_timestamp.astimezone(local_tz)
-    local['header']['eventTimestamp'] = event_timestamp_local.strftime(date_format)
-    return local
+    search_pattern = "(date|datetime|timestamp)"
+    flat = morph.flatten(data)
+    local_flat = copy.deepcopy(flat)
+    local_tz = tz.gettz(get_tenant_timezone(flat["header.tenantId"])) 
+    for k, v in flat.items():
+        key = get_key_tail(k)
+        res = re.search(search_pattern, key, re.IGNORECASE)
+        if (res):
+            parent = get_key_parent(k)
+            local_flat[parent + "." + key + "UTC"] = v
+            local_flat[k] = utc_to_local(v, local_tz)
+    return morph.unflatten(local_flat)
 
 
 def lambda_handler(event: Any, _) -> Dict[str, list]:
